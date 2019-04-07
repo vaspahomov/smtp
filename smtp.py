3880ac88e11bb49e84adb24de0045ad73c4812d8
vaspahomov <vas2142553@gmail.com>
2019-04-07 22:09:23 +0500

    add message config files

--- smtp.py
+++ smtp.py
@@ -5,9 +5,11 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('user.ini')
+config.read('message.ini')
 
 HOST = 'smtp.yandex.ru'
 PORT = 465
+
 LOGIN = config['DEFAULT']['LOGIN']
 PASSWORD = config['DEFAULT']['PASSWORD']
 
@@ -28,8 +30,6 @@ def create_message(login: str,
         f'{message_text}\n'
         f'.'
     )
-    # create message
-    # обрабатывать точки
 
 
 def send_message(sock: socket.socket,
@@ -38,7 +38,7 @@ def send_message(sock: socket.socket,
                  text: str):
     print(send_command(sock, b'MAIL FROM:' + LOGIN.encode()))
 
-    print(send_command(sock, b'RCPT TO:' + recipient.encode()))
+    print(send_command(sock, b'RCPT TO:' + recipient.encode('cp1251')))
     print(send_command(sock, b'DATA'))
 
     message = create_message(
@@ -48,7 +48,7 @@ def send_message(sock: socket.socket,
         text
     )
 
-    print(send_command(sock, message.encode()))
+    print(send_command(sock, message.encode('cp1251')))
 
 
 def login_in(sock: socket.socket):
@@ -60,10 +60,14 @@ def login_in(sock: socket.socket):
 
 
 if __name__ == '__main__':
-    # send_message()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock = ssl.wrap_socket(sock)
         sock.connect((HOST, PORT))
 
         login_in(sock)
-        send_message(sock, LOGIN, 'hello', 'world')
+
+        with open('message.txt') as f:
+            send_message(sock,
+                         config['MESSAGE']['RECIPIENT'],
+                         config['MESSAGE']['SUBJECT'],
+                         f.read())
